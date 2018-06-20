cefa639df1ff3c6fa47a35bf32132448c8e48862
wonikjang <wj2216@columbia.edu>
2018-06-21 00:13:04 +0900

    Add files via upload

---
+++ UserCell.swift
@@ -0,0 +1,21 @@
+//
+//  UserCell.swift
+//  InstagramLike
+//
+//  Created by Wonik Jang on 6/20/18.
+//  Copyright © 2018 Jang&Kim. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var userID: String!
+    
+    
+    
+    
+}
